b21cee399892e7b2c286d4980c74ba2f18b63a0e
ramgaralf <32961754+ramgaralf@users.noreply.github.com>
2026-07-02 20:21:25 +0200

    Version 1.0.1 (#26)

--- frontend/android-app/app/build.gradle.kts
+++ frontend/android-app/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -27,6 +27,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk.debugSymbolLevel = "FULL"
         }
     }
 
